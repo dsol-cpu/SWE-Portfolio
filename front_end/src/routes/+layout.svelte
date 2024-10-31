9563c295dfdf7230f8da5dfe1ecb1c366abb635e
dsol-cpu <dsolinsky98@gmail.com>
2024-10-31 03:30:33 -0400

    added thumbnail metadata

--- front_end/src/routes/+layout.svelte
+++ front_end/src/routes/+layout.svelte
@@ -3,4 +3,22 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- Basic Open Graph Meta Tags for Preview Thumbnail -->
+	<meta property="og:title" content="David Solinsky's software engineer portfolio website" />
+	<meta property="og:description" content="David Solinsky's software engineer portfolio website" />
+	<meta property="og:image" content="portfolio_website_icon.svg" />
+	<meta property="og:url" content="https://dsol-cpu.github.io/SWE-Portfolio" />
+	<meta property="og:type" content="website" />
+
+	<!-- Optional Twitter Card Tags -->
+	<meta name="twitter:card" content="portfolio_website_icon.svg" />
+	<meta name="twitter:title" content="David Solinsky's software engineer portfolio website" />
+	<meta
+		name="twitter:description"
+		content="David Solinsky's software engineer portfolio website."
+	/>
+	<meta name="twitter:image" content="portfolio_website_icon.svg" />
+</svelte:head>
+
 {@render children()}
